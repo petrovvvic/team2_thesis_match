a6a96a313bd4ed9502efe4c521a95ffaeb69458c
aya23 <aya.23madarati2002@gmail.com>
2026-06-14 17:06:38 +0200

    search form für proffeed

--- forms.py
+++ forms.py
@@ -1,15 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField, HiddenField, BooleanField, SelectField
+from wtforms.fields import StringField, SubmitField, HiddenField, BooleanField, SelectField, SearchField
 from wtforms.validators import InputRequired, Length
 
-class CreateTodoForm(FlaskForm):
-    description = StringField(validators=[InputRequired(), Length(min=5)])
-    submit = SubmitField('Create')
+class ProfSearchForm(FlaskForm):
+    search = SearchField(validators=[Optional()])
+    submit = SubmitField('Suchen')
 
-class TodoForm(FlaskForm):
-    method = HiddenField()
-    id = HiddenField()
-    complete = BooleanField()
-    description = StringField(validators=[InputRequired()])
-    list_id = SelectField(coerce=int, choices=[], validate_choice=False)  # (3.)
-    submit = SubmitField('Update')
