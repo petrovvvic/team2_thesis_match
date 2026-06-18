4d3f8664af4db011114b48769419a7647febdec1
aya23 <aya.23madarati2002@gmail.com>
2026-06-18 12:30:53 +0200

    db verbindung + suchlogik

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app.config.from_mapping(
     BOOTSTRAP_BOOTSWATCH_THEME = 'pulse'
 )
 
+from db import db, ProfessorProfile
+from forms import ProfSearchForm
+
 bootstrap = Bootstrap5(app)
 
 @app.route('/')
@@ -16,14 +19,19 @@ def index():
     return redirect(url_for('feed'))
 
 @app.route('/feed/', methods=['GET', 'POST'])
-def feed(): 
-    return render_template('feed.html') 
-   
+def feed():
+    form = forms.ProfSearchForm()
+    professors = db.session.execute(db.select(ProfessorProfile)).scalars().all()
+    if request.method == 'POST' and form.validate():
+        duchbegriff = form.search.data.lower()
+    
+    professors =[ prof for prof in professors
+                    if suchbegriff in f"{prof.user.first_name} {prof.user.last_name}".lower()
+                    or (prof.research_areas and suchbegriff in prof.research_areas.lower())]
+    return render_template('feed.html', form=form, professoren=professors)
+    
 
 @app.route('/profile/<int:id>', methods=['GET', 'POST'])
 def profile(id):
-    return 'profile-detail'
-   
-      
-   
-
+    prof = db.session.get(ProfessorProfile, id)
+    return render_template('profile-detail.html', prof=prof)
