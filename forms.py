e5b71fdcea4614bbfc16280be40188231aac63b7
aya23 <aya.23madarati2002@gmail.com>
2026-06-23 12:36:22 +0200

    faculty filter gelöscht

--- forms.py
+++ forms.py
@@ -106,7 +106,6 @@ class RequestStatusForm(FlaskForm):
 # Screen 2: Professor-Feed Suche/Filter
 class ProfSearchForm(FlaskForm):
     search = SearchField(validators=[Optional()])
-    faculty = SelectField(coerce=str, choices=[], validate_choice=False)
     facheinheit = SelectField(coerce=str, choices=[], validate_choice=False)
     availibilty = SelectField(coerce=str, choices=[('', 'Alle'), ('1', 'Verfügbar'), ('0', 'Nicht verfügbar')], validate_choice=False)
     submit = SubmitField('Suchen')
